fba28a9e2693ac5d79af4943076ca1ed04f05614
TheBoolean-Boy <saswatrath02@gmail.com>
2025-09-10 17:48:37 +0530

    Dark Mode and Shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
 
 export default function Home() {
   return (
-    <div className=" m-4">
-    
-      <SignedOut>
-        <SignInButton mode="modal" >
-          <Button className=" cursor-pointer" variant={"ghost"}>Sign In</Button>
-        </SignInButton>
-        <SignUpButton mode="modal" >
-          <Button className=" cursor-pointer" variant={"default"}>
-            Sign Up
-          </Button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
+    <div className=" m-4 ">
+    Home Page Content
     </div>
   );
 }

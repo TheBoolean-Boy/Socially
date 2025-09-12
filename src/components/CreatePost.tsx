b383a5d532748c551e9e96ba2e2683c322facf50
TheBoolean-Boy <saswatrath02@gmail.com>
2025-09-12 16:31:58 +0530

    Prod build 1

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -8,6 +8,7 @@ import { Button } from "./ui/button";
 import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
 import { createPost } from "@/actions/post.action";
 import toast from "react-hot-toast";
+import ImageUpload from "./ImageUpload";
 
 function CreatePost(){
   const {user} = useUser();
@@ -56,6 +57,18 @@ function CreatePost(){
           </div>
 
           {/* TODO: HANDLE IMAGE UPLOADS */}
+          {(showImageUpload || imageUrl) && (
+            <div className="border rounded-lg p-4">
+              <ImageUpload
+                endpoint="postImage"
+                value={imageUrl}
+                onChange={(ufsUrl) => {
+                  setImageUrl(ufsUrl);
+                  if (!ufsUrl) setShowImageUpload(false);
+                }}
+              />
+            </div>
+          )}
 
           <div className="flex items-center justify-between border-t pt-4">
             <div className="flex space-x-2">
